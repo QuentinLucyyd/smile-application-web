a2a1854c7139d3b63d4132cb45922e07a7cace43
Eduard CLOETE <ecloete@e4r8p9.wethinkcode.co.za>
2018-11-05 09:36:23 +0200

    changed var names in checkin

--- src/app/components/pages/page-checkin/page-checkin.component.ts
+++ src/app/components/pages/page-checkin/page-checkin.component.ts
@@ -7,11 +7,11 @@ import { Options } from 'ng5-slider';
   styleUrls: ['./page-checkin.component.scss']
 })
 export class PageCheckinComponent implements OnInit {
-  value: number = 50;
-  value1: number = 50;
-  value2: number = 50;
-  value3: number = 50;
-  value4: number = 50;
+  p_energy: number = 50;
+  h_connection: number = 50;
+  m_focus: number = 50;
+  f_connected: number = 50;
+  happy: number = 50;
   options: Options = {
     floor: 0,
     ceil: 100,
@@ -28,3 +28,4 @@ export class PageCheckinComponent implements OnInit {
   }
 
 }
+ 
