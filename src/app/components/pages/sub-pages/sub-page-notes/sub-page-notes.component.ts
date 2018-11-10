29ba5f995acedca21b593a51168c7a5d745340a9
eduardclt <eduardcloete@gmail.com>
2018-11-10 15:52:48 +0200

    added note page

---
+++ src/app/components/pages/sub-pages/sub-page-notes/sub-page-notes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sub-page-notes',
+  templateUrl: './sub-page-notes.component.html',
+  styleUrls: ['./sub-page-notes.component.scss']
+})
+export class SubPageNotesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
